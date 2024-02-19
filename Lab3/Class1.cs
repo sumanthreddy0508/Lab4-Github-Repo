f61e2bd525d4bb666376eadde75fac8f13e4b5b1
suman <suman@DESKTOP-SHR8H2U>
2024-02-19 13:56:44 -0500

    Add project files.

---
+++ Lab3/Class1.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace NUnitDemonstration.Tests
+{
+    [TestFixture]
+    public class TriangleTests
+    {
+        [Test]
+        public void ValidTriangle_ValidInput_ReturnsValidMessage()
+        {
+            // Arrange
+            int firstAngle = 60;  // Sumanth Reddy Id:8990462
+            int secondAngle = 60;
+            int thirdAngle = 60;
+            // Act
+            string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("The triangle is valid.", result);
+        }
+        [Test]
+        public void ValidTriangle_InvalidInput_ReturnsInvalidMessage()
+        {
+            // Arrange
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+            // Act
+            string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual("The triangle is valid.", result);
+        }
+        [Test]
+        public void ValidTriangle_InvalidInput_ReturnsInvalidMessage2()
+        {
+            // Add another test
+        }
+    }
+}
+
